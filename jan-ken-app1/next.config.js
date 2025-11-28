5daee117e762fffed71cac27560bb640a1e001c7
deepnix1 <deepnixforwhat@gmail.com>
2025-11-28 12:23:02 +0300

    fix: Allow Farcaster domains in frame-ancestors CSP - fix iframe embedding issue

--- jan-ken-app1/next.config.js
+++ jan-ken-app1/next.config.js
@@ -43,10 +43,8 @@ const nextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload'
           },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
+          // X-Frame-Options removed - using CSP frame-ancestors instead
+          // Farcaster Mini Apps need to be embeddable in Farcaster clients
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff'
